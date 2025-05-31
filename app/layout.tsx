ea991738abbea4ff133f1029e77311a12bc04ecf
Shubham Mishra <shubhammishra10101998@gmail.com>
2025-05-31 17:55:46 +0545

    added lightmode and headerFooter to the codebase

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,44 @@
-import type { Metadata } from 'next';
-import './globals.css';
-import Footer from '@/components/Footer';
-import Navbar from '@/components/Navbar';
+import type { Metadata } from 'next'
+import './globals.css'
+import Providers from '@/components/providers'
+import { Inter, Playfair_Display } from 'next/font/google'
+import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
+import { cn } from '@/lib/utils'
+
+const inter = Inter({ subsets: ['latin'], variable: '--font-sans' })
+const playfair = Playfair_Display({
+  subsets: ['latin'],
+  variable: '--font-serif'
+})
 
 export const metadata: Metadata = {
   title: 'Person Portfolio',
-  description: 'Person Portfolio Created By Shubham Mishra',
-};
+  description: 'Person Portfolio Created By Shubham Mishra'
+}
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body>
-        {/* ========== NAVBAR ========== */}
-        <Navbar />
-
-        <main className="min-h-screen p-6">{children}</main>
-
-        {/* ============= FOOTER ============= */}
-        <Footer />
+    <html lang='en' suppressHydrationWarning>
+      <body
+        className={cn(
+          'flex min-h-screen w-screen flex-col font-sans antialiased',
+          inter.variable,
+          playfair.variable
+        )}
+      >
+        <Providers>
+          <Navbar />
+          <main className='xxl:px-16 grow px-6 md:px-8 lg:px-12'>
+            {children}
+          </main>
+          <Footer />
+        </Providers>
       </body>
     </html>
-  );
+  )
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,32 @@
-import Link from 'next/link';
+import Link from 'next/link'
+import ThemeToggle from './theme-toggle'
 
 export default function Navbar() {
   return (
-    <header className="p-4 flex justify-between">
-      <div className="text-xl font-bold">My Website</div>
-      <nav className="flex gap-4">
-        <Link href="/">Home</Link>
-        <Link href="/blog">Blog</Link>
-        <Link href="/contact">Contact</Link>
+    <header className='bg-background/75 fixed inset-x-0 top-0 z-50 py-6 backdrop-blur-sm px-6 md:px-8 lg:px-12 xxl:px-16'>
+      <nav className='container flex items-center justify-between max-w-screen'>
+        <div>
+          <Link href='/' className='font-serif text-2xl font-bold'>
+            SM
+          </Link>
+        </div>
+
+        <ul className='text-muted-foreground flex items-center gap-6 text-sm font-light sm:gap-10'>
+          <li className='hover:text-foreground transition-colors'>
+            <Link href='/posts'>Posts</Link>
+          </li>
+          <li className='hover:text-foreground transition-colors'>
+            <Link href='/projects'>Projects</Link>
+          </li>
+          <li className='hover:text-foreground transition-colors'>
+            <Link href='/contact'>Contact</Link>
+          </li>
+        </ul>
+
+        <div>
+          <ThemeToggle />
+        </div>
       </nav>
     </header>
-  );
+  )
 }
